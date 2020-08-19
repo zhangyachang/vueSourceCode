429e83d78d07942c9011a382c3f9a62337629e0f
zhangdada <937741304@qq.com>
2020-08-19 15:06:13 +0800

    更新了算法笔记

--- 复习目录/算法/js/sort.js
+++ 复习目录/算法/js/sort.js
@@ -381,7 +381,7 @@ let sort = (function () {
             let left = [];
             let right = [];
 
-            console.log('推荐人', privo);
+            // console.log('推荐人', privo);
 
             for (let i = 0; i < arr.length; i++) {
                 if (arr[i] < privo) {
@@ -393,13 +393,218 @@ let sort = (function () {
 
             console.log(left, right, arr);
             return this.QuickSort3(left).concat([privo], this.QuickSort3(right));
-        }
+        },
+
+        /** 堆排序 */
+        HeapSort(arr) {
+            console.log('堆排序', arr);
+
+            /**
+             * 建堆：维护堆的性质
+             * @param {Array} arr 数组
+             * @param {Number} x 数组下标
+             * @param {Number} len 堆大小
+             */
+            function heapify(arr, x, len) {
+                // console.log('x', x);
+                let l = 2 * x + 1; // 左下标
+                let r = 2 * x + 2; // 右下标
+                let largest = x; // 默认最大的是父节点
+                let temp; // 用于交换数据存储的中间值
+
+                // 寻找一个堆中的最大值
+                if (l < len && arr[l] > arr[largest]) {
+                    largest = l;
+                }
+
+                if (r < len && arr[r] > arr[largest]) {
+                    largest = r;
+                }
+
+                // console.error('largest', largest);
+                // 如果顶部不是最大的就换位置
+                if (largest !== x) {
+                    temp = arr[largest];
+                    arr[largest] = arr[x];
+                    arr[x] = temp;
+
+                    // 递归寻找,维持平衡
+                    heapify(arr, largest, len);
+                }
+            }
+
+            // 构造堆数据结构
+            let heapSize = arr.length;
+            // let maxHeapIndex = ;
+            for (let i = Math.floor(heapSize / 2) - 1; i >= 0; i--) {
+                // debugger
+                heapify(arr, i, heapSize);
+            }
+
+            // console.log('构造堆', arr);
+
+            // 现在要把最大的放到最后一个，最后一个放到第一个，把堆的大小减少一个，在慢慢的把最大的循环上去
+            for (let j = heapSize - 1; j >= 1; j--) {
+                temp = arr[0];
+                arr[0] = arr[j];
+                arr[j] = temp;
+                heapify(arr, 0, --heapSize);
+            }
+            return arr;
+        },
+
+        /** 计数排序 */
+        //  此方法没有参考其他的，后面需要自己再看看
+        CountingSort(arr) {
+            console.log('计数排序', arr);
+
+            let len = arr.length;
+            let min = max = arr[0];
+            let B = [];
+            let C = [];
+
+            // 寻找最大值和最小值
+            for (let i = 0; i < len; i++) {
+                min = min > arr[i] ? arr[i] : min;
+                max = max < arr[i] ? arr[i] : max;
+                B[arr[i]] = B[arr[i]] ? B[arr[i]] + 1 : 1;
+            }
+
+            // console.log('最大值，最小值', max, min);
+            // console.log('初始化完成之后', JSON.stringify(B));
+            // console.log(B);
+
+            for (let i = 0; i < B.length; i++) {
+                console.log('遍历', B[i]);
+                for (let j = 0; j < B[i]; j++) {
+                    C.push(i);
+                }
+            }
+            return C;
+        },
+
+        CountingSort2(arr) {
+            console.log('计数排序2');
+
+            let length = arr.length;
+            let min = max = arr[0];
+            let B = [];
+            let C = [];
+            for (let i = 0; i < length; i++) {
+                min = arr[i] > min ? min : arr[i];
+                max = arr[i] > max ? arr[i] : max;
+                C[arr[i]] ? ++C[arr[i]] : (C[arr[i]] = 1);
+            }
+            console.log('初始化完成', C);
+
+            for (let i = min; i <= max; i++) {
+                console.log('遍历C', C[i]);
+                while (C[i] && C[i] > 0) {
+                    B.push(i);
+                    C[i]--;
+                }
+            }
+            return B;
+        },
+
+        /**
+         * 桶排序
+         * @param {Array} arr 数组
+         * @param {Number} num 桶的个数
+         */
+        BucketSort(arr, num) {
+            console.log('桶排序');
+            if (arr.length <= 1) {
+                return arr;
+            }
+
+            let length = arr.length;
+            let buckets = [];
+            let result = [];
+            let min = max = arr[0];
+            let regex = /^[1-9]+[0-9]*$/;
+            let space;
+            let n = 0;
+
+            console.log('排序长度', length);
+
+            // 定义桶的长度
+            num = num || (num > 1 && regex.test(num) ? num : 10);
+            console.log('桶的长度', num);
+
+            // 寻找最大值和最小值
+            for (let i = 0; i < length; i++) {
+                min = (min <= arr[i]) ? min : arr[i];
+                max = (max >= arr[i]) ? max : arr[i];
+            }
+
+            console.log('最大值', max, '最小值', min);
+            space = (max - min + 1) / num;
+            console.log('间隔是多少', space, length);
+
+            for (let j = 0; j < length; j++) {
+                let index = Math.floor((arr[j] - min) / space);
+                if (buckets[index]) {// 非空桶，插入排序
+                    let key = arr[j];
+                    let k = buckets[index].length - 1;
+                    while (k >= 0 && buckets[index][k] > key) {
+                        buckets[index][k + 1] = buckets[index][k];
+                        k--;
+                    }
+                    buckets[index][k + 1] = key;
+
+                } else {
+                    buckets[index] = [arr[j]]; // 将数据放到第 0 项
+                }
+            }
 
 
+            console.log('当前桶的内容', buckets);
 
+            // 遍历桶返回数组
+            while (n < num) {
+                buckets[n] && (result = result.concat(buckets[n]));
+                n++;
+            }
+            return result;
+        },
 
+        /** 基数排序 */
+        /**
+         * @param {Number} maxDigit 最大位数
+         */
+        RadixSort(arr, maxDigit) {
+            console.log('基数排序', arr);
 
+            let mod = 10;
+            let dev = 1;
+            let counter = [];
 
+            let length = arr.length;
+
+            for (let i = 0; i < maxDigit; i++, mod *= 10, dev *= 10) {
+                for (let j = 0; j < length; j++) {
+                    let num = parseInt((arr[j] % mod) / dev);
+                    if (counter[num]) {
+                        counter[num][counter[num].length] = arr[j];
+                    } else {
+                        counter[num] = [arr[j]];
+                    }
+                }
+                let num = 0;
+                arr = [];
+                // console.log(JSON.stringify(counter));
+                console.log('counter.length', counter.length);
+                while (num <= counter.length) {
+                    for (let k = 0; counter[num] && k < counter[num].length; k++) {
+                        arr[arr.length] = counter[num][k];
+                    }
+                    num++;
+                }
+                counter = [];
+            }
+            return arr;
+        },
     }
 })();
 
